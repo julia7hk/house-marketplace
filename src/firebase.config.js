e09c299f45983b41f9f31482da71f02d5623ac22
Julia Kang <julia7hk@gmail.com>
2022-09-18 15:24:54 -0700

    created react app and installed firebase and installed resources

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app"';
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjEjY3mt3cznZNiRiGIPRJc9qkzZXLzIU",
+  authDomain: "house-marketplace-app-46e6f.firebaseapp.com",
+  projectId: "house-marketplace-app-46e6f",
+  storageBucket: "house-marketplace-app-46e6f.appspot.com",
+  messagingSenderId: "828865805423",
+  appId: "1:828865805423:web:9ba33aec6ce12fee80e19a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
