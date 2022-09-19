c8afaa09a801318806257a190ed350ed79c53402
Julia Kang <julia7hk@gmail.com>
2022-09-18 17:06:14 -0700

    added email signin and signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -21,6 +23,26 @@ function SignUp() {
         }))
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+            const user = userCredential.user
+            
+            updateProfile(auth.currentUser, {
+                displayName: name
+            })
+
+            navigate('/')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 
     return (
       <>
@@ -28,7 +50,7 @@ function SignUp() {
             <header>
                 <p className='pageHeader'>welcome back!</p>
             </header>
-            <form>
+            <form onSubmit={onSubmit}>
                 <input type='text' className='nameInput' placeholder='Name' id='name' value={name} onChange={onChange} />
                 <input type='email' className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange} />
                 <div className='passwordInputDiv'>
@@ -38,9 +60,9 @@ function SignUp() {
 
                 <Link to='/forgot-password' className='forgotPasswordLink'>forgot password</Link>
                 
-                <div className='SignUpBar'>
-                    <p className='SignUpText'>Sign In</p>
-                    <button className="SignUpButton">
+                <div className='signUpBar'>
+                    <p className='signUpText'>Sign Up</p>
+                    <button className="signUpButton">
                         <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
                     </button>
                 </div>
