26953a223bedae763db6444f7ed93f37180d5eb9
Julia Kang <julia7hk@gmail.com>
2022-09-24 14:47:20 -0700

    catch signin errors

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -18,6 +19,26 @@ function SignIn() {
         }))
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+
+            const userCredential = await signInWithEmailAndPassword
+            (auth, email, password)
+
+            if (userCredential.user) {
+                navigate('/')
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+        
+    }
+
 
     return (
       <>
@@ -25,7 +46,8 @@ function SignIn() {
             <header>
                 <p className='pageHeader'>welcome back!</p>
             </header>
-            <form>
+
+            <form onSubmit={onSubmit}>
                 <input type='email' className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange} />
                 <div className='passwordInputDiv'>
                     <input type={showPassword ? 'text' : 'password'} className='passwordInput' placeholder='Password' id='password' value={password} onChange={onChange} />
