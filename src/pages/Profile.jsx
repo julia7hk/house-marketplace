c07d4ef68438cb0666f0d390a13ca64750ee3eac
Julia Kang <julia7hk@gmail.com>
2022-09-24 14:56:41 -0700

    show user name in profile page if logged in

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,16 @@
+import { useState, useEffect } from 'react';
+import { getAuth } from 'firebase/auth'
+
 function Profile() {
-    return (
-      <>
-        <h1>my Profile</h1>
-      </>
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+
+  return (
+      user ? <h1>{user.displayName}</h1> : "Not Logged In"
     );
   }
   
